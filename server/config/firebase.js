653abc581a8af2a2fce7e150b92cf00871d557a9
rishi-bathija <bathijarishi@gmail.com>
2024-09-29 15:08:21 +0530

    service key modified

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -4,7 +4,7 @@ const serviceAccount = {
     type: "service_account",
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY, // Replace escaped newlines
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -13,6 +13,8 @@ const serviceAccount = {
     client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
     "universe_domain": process.env.FIREBASE_UNIVERSAL_DOMAIN
 }
+
+console.log('privatekey', serviceAccount.private_key);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
